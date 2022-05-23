715ae462ae5e747acf6dd2cd3e4e76c75982ab86
Chloebert <berthier.chloe@wanadoo.fr>
2022-05-23 11:34:33 +0200

    quête 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -15,4 +15,11 @@ class ProgramController extends AbstractController
             'website' => 'Wild Series',
          ]);
     }
+
+    #[Route('/program/{id}', requirements: ['id'=>'\d+'], methods: ['GET'], name: 'program_show')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
+    }
+    
 }
