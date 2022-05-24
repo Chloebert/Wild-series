224911e0451efc81c4e1602286e94632a6440c64
Chloebert <berthier.chloe@wanadoo.fr>
2022-05-24 11:48:42 +0200

    quête 10

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,39 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use doctrine\persistence\ManagerRegisty;
+use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
+
+#[Route('/category', name: 'category_')]
+class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+         ]);
+    }
+
+    #[Route('/{categoryName}', methods: ['GET'], name: 'show')]
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
+    {
+        $category = $categoryRepository->findOneByName($categoryName);
+
+        if (!$category) {
+            throw $this->createNotFoundException('No category with name : '.$categoryName. ' found in category\'s table.');
+        }
+
+        $programs = $programRepository->findBy(['category' => $category],['id' => 'DESC'], 3);
+        
+        return $this->render('category/show.html.twig', ['programs' => $programs, 'category' => $category,]);
+    }
+    
+}
