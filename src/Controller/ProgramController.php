9abedd5308a963cc600658c42df92c4679163022
Chloebert <berthier.chloe@wanadoo.fr>
2022-05-27 10:19:08 +0200

    quête 15

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -39,7 +39,7 @@ class ProgramController extends AbstractController
         // Get data from HTTP request
         $form->handleRequest($request);
         // Was the form submitted ?
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $programRepository->add($program, true);
 
             // Redirect to categories list
